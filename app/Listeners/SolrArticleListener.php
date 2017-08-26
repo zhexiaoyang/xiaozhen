8792d103b56c9752863e9fdd3c52b0bb7f1a9bf0
zhexiaoyang <zhangzhen@yeah.net>
2017-08-26 14:56:35 +0800

    修改事件同步Solr索引

---
+++ app/Listeners/SolrArticleListener.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SnycSolrArticleEvent;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SolrArticleListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  SyncSolrArticleEvent  $event
+     * @return void
+     */
+    public function handle(SnycSolrArticleEvent $event)
+    {
+        $article = Article::find($event->id);
+        if (!empty($article))
+        {
+//            dd(gmdate('Y-m-d\TH:i:s\Z',strtotime($article->created_at)));
+            $category = Category::find($article->cid);
+            $options = config('solr.options');
+            $options['path'] = config('solr.article_path');
+            $client = new \SolrClient($options);
+            $client->deleteById($event->id);
+            $client->commit();
+            $document = new \SolrInputDocument();
+            $document->addField('id',$article->id);
+            $document->addField('img_url',$article->img_url);
+            $document->addField('created_at',gmdate('Y-m-d\TH:i:s\Z',strtotime($article->created_at)));
+            $document->addField('article_title',$article->title);
+            $document->addField('article_content',$article->content);
+            $document->addField('article_description',$article->description);
+            $document->addField('category_id',$category->id);
+            $document->addField('category_name',$article->name);
+            $client->addDocument($document);
+            $client->commit();
+        }
+    }
+}
